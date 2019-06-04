cfef8f807b1aaa90d8f65ac0235a24898a741c56
unknown <boom.jaidee@hotmail.com>
2019-06-04 17:59:35 +0700

    push

--- src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
+++ src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
@@ -11,15 +11,22 @@ export interface Food {
   styleUrls: ['./pricing-raw-detail.component.scss']
 })
 export class PricingRawDetailComponent implements OnInit {
+
   foods: Food[] = [
     { value: 'steak-0', viewValue: 'Steak' },
     { value: 'pizza-1', viewValue: 'Pizza' },
     { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
+  displayedColumns = ['date', 'lots', 'Price (USD/MT)', 'Manage'];
+  transactions: Array<any> = [
+    { date: '23/05/2018', lots: 60, Price: 341.9 },
+    { date: '23/05/2018', lots: 60, Price: 341.6 }
+
   ];
   constructor(
   ) { }
 
   ngOnInit() {
   }
-
+ 
 }
