aa29ec5409eba0c71ba0c07604f1f6558a4272d2
unknown <boom.jaidee@hotmail.com>
2019-06-04 13:57:16 +0700

    push

--- src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
+++ src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-pricing-raw-detail',
   templateUrl: './pricing-raw-detail.component.html',
   styleUrls: ['./pricing-raw-detail.component.scss']
 })
 export class PricingRawDetailComponent implements OnInit {
-
+ 
   constructor(
   ) { }
 
--- src/app/main/pricing-raw/pricing-raw.module.ts
+++ src/app/main/pricing-raw/pricing-raw.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { MatSelectModule } from '@angular/material/select';
 import { FuseSharedModule } from '@fuse/shared.module';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule, MatPaginatorModule, MatSortModule, } from '@angular/material'
 import { PricingRawListComponent } from './pricing-raw-list/pricing-raw-list.component';
 import { PricingRawDetailComponent } from './pricing-raw-detail/pricing-raw-detail.component';
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
 const routes = [
   {
     path: 'pricing-raw',
@@ -26,12 +28,14 @@ const routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
-
+    MatFormFieldModule,
     TranslateModule,
-
+    MatSelectModule,
     FuseSharedModule,
     MatGridListModule,
+    MatInputModule,
     MatTableModule,
+    FormsModule,
     MatPaginatorModule,
     MatSortModule
   ],
