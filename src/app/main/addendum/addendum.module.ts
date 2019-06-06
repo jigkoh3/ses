15d9bb6e449d6a0005c0c2208821b535f6e5b493
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-06-06 15:28:41 +0700

    as

--- src/app/main/addendum/addendum.module.ts
+++ src/app/main/addendum/addendum.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddendumListComponent } from './addendum-list/addendum-list.component';
 import { RouterModule } from '@angular/router';
+import { MatMenuModule, MatExpansionModule, MatIconModule, MatCardModule, MatButtonModule, MatSortModule, MatPaginatorModule, MatTableModule, MatInputModule, MatGridListModule, MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes = [
   {
@@ -15,6 +19,21 @@ const routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    MatFormFieldModule,
+    TranslateModule,
+    MatSelectModule,
+    FuseSharedModule,
+    MatGridListModule,
+    MatInputModule,
+    MatTableModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatMenuModule
   ],
   exports:[
     AddendumListComponent
