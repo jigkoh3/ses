7748c696d1385ffcbc644a585dfebc14d7e0f1c0
pure1234567a <pure1234567a@gmail.com>
2019-06-06 11:36:50 +0700

    p

---
+++ src/app/main/pricing-white/pricing-white-detail/pricing-white-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-white-detail',
+  templateUrl: './pricing-white-detail.component.html',
+  styleUrls: ['./pricing-white-detail.component.scss']
+})
+export class PricingWhiteDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
