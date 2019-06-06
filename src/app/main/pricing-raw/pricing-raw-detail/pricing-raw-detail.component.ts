475dbd06a1c8fc9dcb73a176f6d770c7d907c8a2
unknown <boom.jaidee@hotmail.com>
2019-06-06 15:25:10 +0700

    pusH

--- src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
+++ src/app/main/pricing-raw/pricing-raw-detail/pricing-raw-detail.component.ts
@@ -14,19 +14,20 @@ export interface Food {
 export class PricingRawDetailComponent implements OnInit {
   dataPricing = {
     status: 200,
-    typeofsugar: 'WHITE SUGAR',
+    typeofsugar: 'Hi-Raw',
     buyer: 'ED & F Man',
     groupfactory: 'tr-group',
+    futuremarket: 'No. 11',
     buyercontractno: 'P29599',
     contractdate: '20/02/2017',
     contractmonth: 'March',
-    qty: 12000,
+    qty: 24000,
     shipment: {
       startdate: '20/02/2018',
       enddate: '20/05/2018'
     },
     contractno: '06818/TR',
-    premium: '16',
+    premium: '0.83',
     contractyear: '2019',
     whitepremium: {
       totalpricingrequest: 5000,
@@ -36,18 +37,16 @@ export class PricingRawDetailComponent implements OnInit {
       year: '2018'
     },
     totalAll: {
-      averageprice: 342.47,
-      other: -0.50,
-      premium: 16,
-      finalprice: 357.97,
-      totalpricing: 240,
-      priced: 238,
-      unpriced: 2
+      averageprice: 14.983,
+      other: -0.02,
+      premium: 0.83,
+      finalprice: 15.793,
+      equivalent: 348.18,
+      totalpricing: 472,
+      priced: 30,
+      unpriced: 442
     }
   }
-
-
-
   panelOpenState = false;
   hideCol: any;
   breakpoint: any;
@@ -59,6 +58,7 @@ export class PricingRawDetailComponent implements OnInit {
   typeofsugar: Array<any> = [
     { value: 'WHITE SUGAR', viewValue: 'WHITE SUGAR' },
     { value: 'REFINED SUGAR', viewValue: 'REFINED SUGAR' },
+    { value: 'Hi-Raw', viewValue: 'Hi-Raw' }
   ];
   buyer: Array<any> = [
     { value: 'ED & F Man', viewValue: 'ED & F Man' },
@@ -92,41 +92,27 @@ export class PricingRawDetailComponent implements OnInit {
     { value: '2019', viewValue: '2019' },
 
   ]
-  foods: Food[] = [
-    { value: 'steak-0', viewValue: 'Steak' },
-    { value: 'pizza-1', viewValue: 'Pizza' },
-    { value: 'tacos-2', viewValue: 'Tacos' }
-  ];
-  displayedColumns: string[] = ['date', 'lots', 'Price (USD/MT)', 'Manage'];
-  transactions: Array<any> = [
-    { date: '23/05/2018', lots: 60, Price: 341.9 },
-    { date: '23/05/2018', lots: 60, Price: 341.6 }
-  ];
-  buyNewYorkNo11: Array<any> = [
-    { date: '23/05/2018', lots: 60, Price: 12.56 },
-    { date: '06/06/2018', lots: 60, Price: 12.41 },
-    { date: '08/06/2018', lots: -2, Price: 12.3 }
-  ]
-  columnsNewYork: string[] = ['orderdate', 'sell', 'buy', 'against', 'mon', 'year', 'price', 'unit', 'executed', 'Manage']
-  // columnsNewYork: string[] = ['orderdate', 'sell', 'buy', 'against', 'price', 'unit', 'executed', 'Manage']
+ 
   newYorkNo11Table: Array<any> = [
     { orderdate: '17/05/2018', sell: 45, buy: '', against: 'No.11', mon: 'Mar', year: '2018', price: '11.93', unit: 'cents/pound', executed: '17/05/2018' },
     { orderdate: '18/05/2018', sell: 15, buy: '', against: 'No.11', mon: 'Mar', year: '2018', price: '12.00', unit: 'cents/pound', executed: '18/05/2018' },
     { orderdate: '21/05/2018', sell: 30, buy: '', against: 'No.11', mon: 'Mar', year: '2018', price: '12.13', unit: 'cents/pound', executed: '21/05/2018' },
     { orderdate: '23/05/2018', sell: 28, buy: '', against: 'No.11', mon: 'Mar', year: '2018', price: '12.73', unit: 'cents/pound', executed: '23/05/2018' },
   ]
-  columnLondon: string[] = ['orderdate', 'sell', 'buy', 'against', 'mon', 'year', 'price', 'unit', 'executed', 'Manage']
+  columnLondon: string[] = ['orderdate', 'sell', 'buy', 'against', 'mon', 'year', 'price', 'executed', 'Manage']
 
   londonTable: Array<any> = [
-    { orderdate: '24/05/2018', sell: 40, buy: '', against: 'No.5', mon: 'Mar', year: '2018', price: '348.00', unit: 'USD/MT', executed: '24/05/2018' },
-    { orderdate: '25/05/2018', sell: 40, buy: '', against: 'No.5', mon: 'Mar', year: '2018', price: '350.00', unit: 'USD/MT', executed: '25/05/2018' },
-    { orderdate: '26/05/2018', sell: 40, buy: '', against: 'No.5', mon: 'Mar', year: '2018', price: '352.00', unit: 'USD/MT', executed: '26/05/2018' },
+    { orderdate: '18/10/2017', sell: 25, buy: null, against: 'No.11', mon: 'Mar', year: '2018', price: '14.90', executed: '27/10/2017' },
+    { orderdate: '18/10/2017', sell: null, buy: 25, against: 'No.11', mon: 'Mar', year: '2018', price: '14.60', executed: '28/10/2017' },
+    { orderdate: '19/10/2017', sell: 20, buy: null, against: 'No.11', mon: 'Mar', year: '2018', price: '14.50', executed: '29/10/2017' },
+    { orderdate: '30/10/2017', sell: null, buy: 20, against: 'No.11', mon: 'Mar', year: '2018', price: '14.60', executed: '29/10/2017' },
+    { orderdate: '31/10/2017', sell: 30, buy: null, against: 'No.11', mon: 'Mar', year: '2018', price: '14.80', executed: '30/10/2017' },
+
   ]
   columnOthers: string[] = ['others', 'amont', 'unit', 'manage']
   othersTable: Array<any> = [
-    { others: 'free', amont: '-0.50', unit: 'USD/MT' },
+    { others: 'free', amont: '-0.02', unit: 'cents/pound' },
     { others: '...', amont: '', unit: '' }
-
   ]
   constructor(
   ) { }
