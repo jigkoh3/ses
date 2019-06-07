45573154045f7fc57fa502797bcc3fe60788f4bd
pure1234567a <pure1234567a@gmail.com>
2019-06-07 15:48:38 +0700

    p

--- src/app/main/contract/contract.module.ts
+++ src/app/main/contract/contract.module.ts
@@ -7,6 +7,10 @@ import { ContractDetailComponent } from './contract-detail/contract-detail.compo
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { FuseSidebarModule } from '@fuse/components';
+import { MatFormFieldModule, MatSelectModule, MatGridListModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule, MatCardModule, MatExpansionModule, MatMenuModule } from '@angular/material';
+import { TranslateModule } from '@ngx-translate/core';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { NgxDataTableModule } from 'ngx-nested-data-table';
 const routes = [
   {
     path: 'contracts',
@@ -26,7 +30,23 @@ const routes = [
     FormsModule,
     ReactiveFormsModule,
     FuseSidebarModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    TranslateModule,
+    FuseSharedModule,
+    MatGridListModule,
+    MatInputModule,
+    MatTableModule,
+    FormsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatMenuModule,
+    NgxDataTableModule
   ],
   exports: [
     ContractListComponent,
