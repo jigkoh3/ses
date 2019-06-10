3a8fc0af3f6b8f34249931c16d56e88b0a60bcf2
jigkoh3 <jigkoh3@hotmail.com>
2019-06-10 22:47:30 +0700

    Odata Config header

--- src/app/ODataConfigurationFactory.ts
+++ src/app/ODataConfigurationFactory.ts
@@ -2,15 +2,32 @@
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { ODataConfiguration } from 'angular-odata-es5/angularODataConfiguration';
+import { RequestOptions } from 'http';
 
 export class ODataConfigurationFactory {
 
-    constructor () {
+    constructor() {
         var currentUser = JSON.parse(localStorage.getItem('SEScurrentUser'));
         const config = new ODataConfiguration();
         //const myconfig = <MyODataConfiguration>config;
         config.baseUrl = environment.serviceUrl;
-        config.postHeaders = new HttpHeaders({ 'Authorization': 'Bearer ' + (currentUser?currentUser.token:""), 'Content-Type': 'application/json; charset=utf-8' });
+        // config.postHeaders = new HttpHeaders({ 'Authorization': 'Bearer ' + (currentUser ? currentUser.token : ""), 'Content-Type': 'application/json; charset=utf-8' });
+
+        config.defaultRequestOptions.headers = new HttpHeaders({
+            'Authorization': 'Bearer ' + (currentUser ? currentUser.token : ""),
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+
+        config.customRequestOptions.headers = new HttpHeaders({
+            'Authorization': 'Bearer ' + (currentUser ? currentUser.token : ""),
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+
+        config.postRequestOptions.headers = new HttpHeaders({
+            'Authorization': 'Bearer ' + (currentUser ? currentUser.token : ""),
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        console.log(config);
         return config;
     }
 }
