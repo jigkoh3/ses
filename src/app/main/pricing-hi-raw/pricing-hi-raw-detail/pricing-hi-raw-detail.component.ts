752b2b7f423bd2e65ab658615fc986b6dd7a7307
jigkoh3 <jigkoh3@hotmail.com>
2019-06-15 08:11:38 +0700

    wwww

--- src/app/main/pricing-hi-raw/pricing-hi-raw-detail/pricing-hi-raw-detail.component.ts
+++ src/app/main/pricing-hi-raw/pricing-hi-raw-detail/pricing-hi-raw-detail.component.ts
@@ -6,6 +6,15 @@ import { ODataConfiguration, ODataExecReturnType, ODataPagedResult, ODataQuery, 
 import { ODataConfigurationFactory } from '../../../ODataConfigurationFactory';
 import { Router } from '@angular/router';
 
+
+
+const ELEMENT_DATA: Array<any> = [
+  { orderdate: '17/05/2018', sell: 45, buy: '', against: 'No.11', 'mon': 'Mar', year: '2018', price: '11.93', unit: 'cents/pound', executed: '17/05/2018' },
+  { orderdate: '18/05/2018', sell: '', buy: 45, against: 'No.11', mon: 'Mar', year: '2018', price: '12.00', unit: 'cents/pound', executed: '18/05/2018' },
+  { orderdate: '21/05/2018', sell: 30, buy: '', against: 'No.11', mon: 'Mar', year: '2018', price: '12.13', unit: 'cents/pound', executed: '21/05/2018' },
+  { orderdate: '23/05/2018', sell: '', buy: 28, against: 'No.11', mon: 'Mar', year: '2018', price: '12.73', unit: 'cents/pound', executed: '23/05/2018' },
+]
+
 @Component({
   selector: 'app-pricing-hi-raw-detail',
   templateUrl: './pricing-hi-raw-detail.component.html',
@@ -17,7 +26,8 @@ import { Router } from '@angular/router';
 export class PricingHiRawDetailComponent implements OnInit {
   form: FormGroup;
 
-
+  displayedColumns: string[] = ['orderdate', 'sell', 'buy', 'against', 'mon', 'year', 'price',  'executed', 'star'];
+  dataSource = ELEMENT_DATA;
   /**
      * Constructor
      *
